b5b36a3d1b23649b7251e3d768ba0cbe9813976b
mwlcx <matt.wilcox94@gmail.com>
2026-08-21 09:44:27 +0100

    Add files via upload

---
+++ dsi-streamlit-webapp.py
@@ -0,0 +1,62 @@
+# Streamlit Web-app
+
+# Import libraries
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+#load our model object
+model = joblib.load("model.joblib")
+
+
+# Add title and instructions
+
+st.title("Purchase Prediction Model")
+st.subheader("Enter customer information and submit for likelihood ot purchase")
+
+# age input form
+age = st.number_input(
+    label = "01. Enter the customer's age",
+    min_value = 18,
+    max_value = 120,
+    value = 35)
+
+#gender input form
+gender = st.radio(
+    label = "02. Enter the custoemr's gender",
+    options = ["M", "F"])
+
+
+# credit score input form
+
+credit_score = st.number_input(
+    label = "03. Enter the customer's credit score",
+    min_value = 0,
+    max_value = 1000,
+    value = 500)
+
+
+# Submit inputs to model
+
+if st.button("Submit For Prediction"):
+    
+    # Store our data in a DF for preiction
+    new_data = pd.DataFrame({"age" : [age], "gender" : [gender], "credit_score" : [credit_score]})
+    
+    # apply model pipeline to the input data and extract probability prediction
+    pred_proba = model.predict_proba(new_data)[0][1]
+    
+    # output prediction
+    st.subheader(f"Based on these customer attributes, our model predicts a purchase probability of {pred_proba:.0%}")
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
